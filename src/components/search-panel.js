0dba5af1707c6065936683341b3200a3efe4b85b
Otonas Gineitis <otonas.gineitis@sweco.lt>
2019-02-18 16:53:04 +0200

    initial

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,14 +1,14 @@
 import React from "react";
 
+import './search-panel.css';
+
 const SearchPanel = () => {
-    const searchText = 'Type to search';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return <input
-        style={searchStyle}
-        placeholder={searchText}
-    />;
+
+    return (
+        <input type="text"
+               className="form-control search-input"
+               placeholder="type to search"/>
+    );
 };
 
 export default SearchPanel;
